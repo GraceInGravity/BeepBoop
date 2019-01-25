6d35e3e858e342bdee62f4a18d5de688fe3b1a6f
nuala <nuala.k.gray@gmail.com>
2019-01-25 15:57:25 -0800

    Add Dave quote at 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
+        // function beepBoop to count up to numberUserTxt
 var beepBoop = function(numberUserTxt){
   var countingUp = [];
-  for (var i = 0; i < numberUserTxt; i++) {
+  for (var i = 0; i < numberUserTxt + 1; i++) {
     countingUp.push(i);
     console.log(i);
-    console.log(countingUp);
+    console.log("counting up = " + countingUp);
   }
-
   return countingUp;
 };
+
+var dave = function(result){
+  for (var i = 0; i < result.length; i++) {
+    if (i=3) {
+      result[i]="Sorry Dave I can't do that.";
+      console.log(result);
+    }
+      console.log("This is final result from dave" + result);
+    return result;
+  }
+}
+
 
 $(document).ready(function() {
 
@@ -19,7 +31,15 @@ $(document).ready(function() {
 
     var result = beepBoop(numberUserTxt);
     $("#result").text(result);
-      console.log(result);
-      console.log(beepBoop);
+      console.log("result = " + result);
+
+    var nextResultDave = [];
+    nextResultDave = dave(result);
+    // $(#nextResultDave).text(nextResultDave);
+    console.log("nextResultDave = " + nextResultDave);
+
   });
+
+
+
 });
